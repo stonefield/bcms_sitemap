1b505b5bc32d4b63d0429449622ab93162137d30
knut <knut.stenmark@gmail.com>
2010-07-30 12:31:27 +0200

    Fixed tests

--- test/unit/helpers/sitemaps_helper_test.rb
+++ test/unit/helpers/sitemaps_helper_test.rb
@@ -7,7 +7,7 @@ class SitemapsHelperTest < ActionView::TestCase
   context 'xml_url' do
     setup do
       page_routes(:new_article).reload_routes
-      @news = create_news_article :name => 'good-news', :created_at => Date.parse('2010-03-03')
+      @news = create_news_article :name => 'good-news' #, :created_at => Date.parse('2010-03-03')
       flunk "Expected news article not to respond to path" if @news.respond_to?(:path)
       @page = create_page :path => '/home'
       flunk "Expected page to respond to path" unless @page.respond_to?(:path)
@@ -16,7 +16,8 @@ class SitemapsHelperTest < ActionView::TestCase
       assert_equal "http://test.host/home", xml_url(@page)
     end
     should 'handle objects which does not have path' do
-      assert_equal "http://test.host/news/articles/2010/03/02/good-news", xml_url(@news)
+      date_path = Date.yesterday.strftime("%Y/%m/%d")
+      assert_equal "http://test.host/news/articles/#{date_path}/good-news", xml_url(@news)
     end
   end
 end
